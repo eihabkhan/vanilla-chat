16abc07a0afa1b9945fe2a795931f72e3c401357
Eihab <mastereihab@gmail.com>
2020-09-02 16:55:15 +0100

    added sending messages functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 // DOM Queries
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+
+// Add New Chat
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+        .then(() => {
+            newChatForm.reset()
+        })
+            .catch(err => console.log(err));
+})
 
 // Class Instances
 const chatUI = new ChatUI(chatList);
