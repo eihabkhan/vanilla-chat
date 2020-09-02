58421683eded1f23c48efe2b3614bbb0de057df2
Eihab <mastereihab@gmail.com>
2020-09-02 18:10:09 +0100

    added channel change functionality

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
 const newNameForm = document.querySelector('.new-name');
 const updateMessage = document.querySelector('.update-message');
+const rooms = document.querySelector('.chat-rooms');
 
 // Add New Chat
 newChatForm.addEventListener('submit', e => {
@@ -24,6 +25,14 @@ newNameForm.addEventListener('submit', e => {
     // Notify user of name update
     updateMessage.innerText = `Your name was updated to "${newName}"`;
     setTimeout(() => updateMessage.innerText = '', 2000);
+});
+
+rooms.addEventListener('click', e => {
+    if (e.target.tagName === 'BUTTON') {
+        chatUI.clear(); 
+        chatroom.updateChannel(e.target.getAttribute('id'));
+        chatroom.getChats(chat => chatUI.render(chat));
+    }        
 });
 
 // Check localStorage for username
--- js/ui.js
+++ js/ui.js
@@ -22,4 +22,8 @@ class ChatUI {
 
         this.list.innerHTML += html
     }
+
+    clear() {
+        this.list.innerHTML = '';
+    }
 }
